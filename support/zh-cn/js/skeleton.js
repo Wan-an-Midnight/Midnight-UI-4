6c6033ae143a2bb098439cddfceb65763f0f1d44
Taohua Nong <112565197+gugubirdNTH@users.noreply.github.com>
2022-11-06 18:13:58 +0800

    Add files via upload

---
+++ support/zh-cn/js/skeleton.js
@@ -0,0 +1,11 @@
+ // * @Author       : Junchen-Yi
+ // * @Designer	    : Taohua-Nong
+ // * @Date         : 2022-07-15 21:11:30
+ // * @Program name : skeleton.js
+ // * Copyright (c) 2022 by Junchen-Yi Taohua-Nong, All Rights Reserved. 
+setTimeout("skeleton()",5000 );  
+function skeleton(){
+	document.getElementById("index-main").style.display = "block";
+	document.getElementById("skeleton").style.display = "none";
+
+}
