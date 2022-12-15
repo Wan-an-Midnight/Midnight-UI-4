bf779fc7f4115b35612086002685a4277cb0e44b
Junchen Yi <63986407+Jimmy-Effe@users.noreply.github.com>
2022-12-15 16:07:17 +0800

    Delete en2 directory

--- en2/js/ranking-data.js
+++
@@ -1,9 +0,0 @@
- // * @Author       : Junchen-Yi
- // * @Designer	    : Taohua-Nong
- // * @Date         : 2022-10-13 13:43:08
- // * @Program name : about.js
- // * Copyright (c) 2022 by Junchen-Yi Taohua-Nong, All Rights Reserved. 
- 
- function ranking_data(){
-	 
- }
