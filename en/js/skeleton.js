6fd77a57aef55238e910f9c72e48d41836f24ea7
Junchen Yi <63986407+Jimmy-Effe@users.noreply.github.com>
2022-12-15 16:23:26 +0800

    public beta  (15A1151g)

---
+++ en/js/skeleton.js
@@ -0,0 +1,10 @@
+ // * @Author       : Junchen-Yi
+ // * @Designer	    : Taohua-Nong
+ // * @Date         : 2022-07-15 21:11:30
+ // * @Program name : skeleton.js
+ // * Copyright (c) 2022 by Junchen-Yi Taohua-Nong, All Rights Reserved. 
+setTimeout("skeleton()",7000 );  
+function skeleton(){
+	
+
+}
