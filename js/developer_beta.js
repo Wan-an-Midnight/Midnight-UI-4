dd59f7a9215203635d8cccfd500732acc9cfd878
Junchen Yi <63986407+Jimmy-Effe@users.noreply.github.com>
2023-01-16 18:18:56 +0800

    developer beta 7 (15A1151g)

---
+++ js/developer_beta.js
@@ -0,0 +1,22 @@
+function ver(){
+	document.getElementById("version").innerHTML="4.0 beta 7";
+	document.getElementById("buildid").innerHTML="15A1151g";
+	
+}
+// function check(){
+// 	setTimeout(function(){alert("已是最新版本!");
+// 	document.getElementById("upnew").style.display = "inherit";
+// 	//document.getElementById("uphave").style.display = "inherit";
+// },2000 );  
+// }
+// function check_en(){
+// 	setTimeout(function(){alert("Is already the latest version!");
+// 	document.getElementById("upnew").style.display = "inherit";
+// 	//document.getElementById("uphave").style.display = "inherit";
+// },2000 );  
+// }
+function index_ver(){
+	document.getElementById("version-nav").innerHTML="Midnight UI 4 Developer Beta 7 (15A1151g)"
+	document.getElementById("fooder-ver").innerHTML="4.0 Beta 7 (15A1151g)"
+	
+}
